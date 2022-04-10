6713151844e98a3cdd0e6c60de13310e2dc87e1d
zhangting <1962857032@qq.com>
2022-04-11 00:02:39 +0800

    Update main.go

--- week04/cmd/book/main.go
+++ week04/cmd/book/main.go
@@ -1,1 +1,69 @@
+package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/pkg/errors"
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	g, ctx := errgroup.WithContext(context.Background())
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	// 访问链接退出
+	serverOut := make(chan struct{})
+	mux.HandleFunc("/shutdown", func(w http.ResponseWriter, r *http.Request) {
+		serverOut <- struct{}{}
+	})
+
+	server := http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+
+	// 第一个协程,启动http监听服务
+	g.Go(func() error {
+		return server.ListenAndServe()
+	})
+
+	// 第二个协程,监听取消以及手动取消
+	g.Go(func() error {
+		select {
+		case <-ctx.Done():
+			log.Println("errgroup exit...")
+		case <-serverOut:
+			log.Println("server will out...")
+		}
+		// 接收到手动取消
+		cancel, cancelFunc := context.WithCancel(ctx)
+		defer cancelFunc()
+		log.Println("shutting down server...")
+		_ = server.Shutdown(cancel)
+		return nil
+	})
+
+	// 第三个协程,接收信号量
+	g.Go(func() error {
+		quit := make(chan os.Signal, 0)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case sig := <-quit:
+			return errors.Errorf("get os signal: %v", sig)
+		}
+	})
+
+	fmt.Printf("errgroup exiting: %+v\n", g.Wait())
